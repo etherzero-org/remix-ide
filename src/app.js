a6dc1a2a027f1e55d774e8649d4516aca3008a9d
Jerry <13087405+supstar0@users.noreply.github.com>
2018-06-11 19:22:39 +0800

    Revert "Alert changed"
    This reverts commit 4a0bb05d37c79fbeee97f72c66f6932bd7a672dd.

--- src/app.js
+++ src/app.js
@@ -214,18 +214,18 @@ function run () {
   var self = this
 
   if (window.location.hostname === 'yann300.github.io') {
-    modalDialogCustom.alert('This UNSTABLE ALPHA branch of Remix has been moved to http://etherzero.github.io/remix-live-alpha.')
+    modalDialogCustom.alert('This UNSTABLE ALPHA branch of Remix has been moved to http://ethereum.github.io/remix-live-alpha.')
   } else if (window.location.hostname === 'remix-alpha.ethereum.org' ||
   (window.location.hostname === 'ethereum.github.io' && window.location.pathname.indexOf('/remix-live-alpha') === 0)) {
     modalDialogCustom.alert(`This instance of the Remix IDE is an UNSTABLE ALPHA branch.\n
-Please only use it if you know what you are doing, otherwise visit the stable version at http://remix.etherzero.org.`)
+Please only use it if you know what you are doing, otherwise visit the stable version at http://remix.ethereum.org.`)
   } else if (window.location.protocol.indexOf('http') === 0 &&
-  window.location.hostname !== 'remix.etherzero.org' &&
+  window.location.hostname !== 'remix.ethereum.org' &&
   window.location.hostname !== 'localhost' &&
   window.location.hostname !== '127.0.0.1') {
-    modalDialogCustom.alert(`The Remix IDE has moved to http://remix.etherzero.org.\n
+    modalDialogCustom.alert(`The Remix IDE has moved to http://remix.ethereum.org.\n
 This instance of Remix you are visiting WILL NOT BE UPDATED.\n
-Please make a backup of your contracts and start using http://remix.etherzero.org`)
+Please make a backup of your contracts and start using http://remix.ethereum.org`)
   }
 
   if (window.location.protocol.indexOf('https') === 0) {
